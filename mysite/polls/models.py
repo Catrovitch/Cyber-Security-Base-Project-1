d4770b57f61336f0e5dbc094e92c1397c5bc45d6
Alexander <ale.engelhardt@outlook.com>
2022-10-24 17:40:03 +0300

    class Account added, db.sqlite3 updated

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Account(models.Model):
+	owner = models.ForeignKey(User, on_delete=models.CASCADE)
+	iban = models.TextField()
+	balance = models.IntegerField()
