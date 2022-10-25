5e71c393a4e00f3a6cf6738fb0473db7169345c7
Alexander <ale.engelhardt@outlook.com>
2022-10-25 17:42:59 +0300

    big update

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,8 +1,25 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
-from django.contrib.auth.models import User
+class Question(models.Model):
 
-class Account(models.Model):
-	owner = models.ForeignKey(User, on_delete=models.CASCADE)
-	iban = models.TextField()
-	balance = models.IntegerField()
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    question_code = int
+	
+    def __str__(self):
+	    return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+    def  was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    datetime.timedelta(days=1)
